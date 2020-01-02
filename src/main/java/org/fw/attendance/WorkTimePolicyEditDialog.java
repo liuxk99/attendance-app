608a2e18b1c19ba5797c8138f7ee3e94623051e6
Thomas Liu <liuxk99@gmail.com>
2020-01-02 14:55:02 +0800

    <feature: WorkTimePolicy/EditorDialog>: interaction.

--- src/main/java/org/fw/attendance/WorkTimePolicyEditDialog.java
+++ src/main/java/org/fw/attendance/WorkTimePolicyEditDialog.java
@@ -7,12 +7,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.sj.attendance.bl.TimeUtils;
 
 public class WorkTimePolicyEditDialog extends AlertDialog implements View.OnClickListener {
-    final String TAG = WorkTimePolicyEditDialog.class.getSimpleName();
+    private final String TAG = WorkTimePolicyEditDialog.class.getSimpleName();
+    private TextView checkInTextView;
+    private TextView latestCheckInTextView;
+    private TextView checkOutTextView;
 
     protected WorkTimePolicyEditDialog(Context context) {
         super(context);
@@ -28,12 +33,27 @@ public class WorkTimePolicyEditDialog extends AlertDialog implements View.OnClic
 
         setContentView(R.layout.work_time_policy_editor_dialog);
 
+        EditText titleEditText = findViewById(R.id.ed_policy_title);
+
         Button checkInButton = findViewById(R.id.btn_modify_time_check_in);
         checkInButton.setOnClickListener(this);
+        checkInTextView = findViewById(R.id.tv_check_in_value);
+
         Button latestCheckInButton = findViewById(R.id.btn_modify_time_latest_check_in);
         latestCheckInButton.setOnClickListener(this);
+        latestCheckInTextView = findViewById(R.id.tv_latest_check_in_value);
+
         Button checkOutButton = findViewById(R.id.btn_modify_time_check_out);
         checkOutButton.setOnClickListener(this);
+        checkOutTextView = findViewById(R.id.tv_check_out_value);
+
+        updateView();
+    }
+
+    private void updateView() {
+        checkInTextView.setText(TimeUtils.formatRefTime(checkInTime));
+        latestCheckInTextView.setText(TimeUtils.formatRefTime(latestCheckInTime));
+        checkOutTextView.setText(TimeUtils.formatRefTime(checkOutTime));
     }
 
     protected WorkTimePolicyEditDialog(Context context, int themeResId) {
@@ -44,15 +64,15 @@ public class WorkTimePolicyEditDialog extends AlertDialog implements View.OnClic
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_modify_time_check_in: {
-                onClickCheckIn();
+                onClickTime(new CheckInTimePickerListener(), checkInTime);
             }
             break;
             case R.id.btn_modify_time_latest_check_in: {
-                onClickLatestCheckIn();
+                onClickTime(new LatestCheckInTimePickerListener(), latestCheckInTime);
             }
             break;
             case R.id.btn_modify_time_check_out: {
-                onClickCheckOut();
+                onClickTime(new CheckOutTimePickerListener(), checkOutTime);
             }
             break;
             default:
@@ -60,55 +80,55 @@ public class WorkTimePolicyEditDialog extends AlertDialog implements View.OnClic
         }
     }
 
-    /*
-     * 实现TimePickerDialog.OnTimeSetListener接口，处理新的时间输入。
-     */
     public class CheckOutTimePickerListener implements TimePickerDialog.OnTimeSetListener {
         public void onTimeSet(TimePicker view, int hour, int minute) {
             Log.i(TAG, "CheckOutTimePickerListener#onTimeSet(" + hour + ", " + minute + ")");
+
+            long time = TimeUtils.compoundTime(hour, minute);
+            if (time != checkOutTime) {
+                checkOutTime = time;
+                checkOutTextView.setText(TimeUtils.formatRefTime(checkOutTime));
+            }
         }
     }
 
-    private void onClickCheckOut() {
-        CheckInTimePickerListener checkInTimePickerListener = new CheckInTimePickerListener();
-        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(),
-                checkInTimePickerListener, 18,
-                0,
-                true);
-        timePickerDialog.show();
-    }
-
-    /*
-     * 实现TimePickerDialog.OnTimeSetListener接口，处理新的时间输入。
-     */
     public class LatestCheckInTimePickerListener implements TimePickerDialog.OnTimeSetListener {
         public void onTimeSet(TimePicker view, int hour, int minute) {
             Log.i(TAG, "LatestCheckInTimePickerListener#onTimeSet(" + hour + ", " + minute + ")");
-        }
-    }
 
-    private void onClickLatestCheckIn() {
-        CheckInTimePickerListener checkInTimePickerListener = new CheckInTimePickerListener();
-        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(),
-                checkInTimePickerListener, 10,
-                0,
-                true);
-        timePickerDialog.show();
+            long time = TimeUtils.compoundTime(hour, minute);
+            if (time != latestCheckInTime) {
+                latestCheckInTime = time;
+                latestCheckInTextView.setText(TimeUtils.formatRefTime(latestCheckInTime));
+            }
+        }
     }
 
     public class CheckInTimePickerListener implements TimePickerDialog.OnTimeSetListener {
         public void onTimeSet(TimePicker view, int hour, int minute) {
             Log.i(TAG, "CheckInTimePickerListener#onTimeSet(" + hour + ", " + minute + ")");
+
+            long time = TimeUtils.compoundTime(hour, minute);
+            if (time != checkInTime) {
+                checkInTime = time;
+                checkInTextView.setText(TimeUtils.formatRefTime(checkInTime));
+            }
         }
     }
 
-    private void onClickCheckIn() {
-        CheckInTimePickerListener checkInTimePickerListener = new CheckInTimePickerListener();
+    private long checkInTime = TimeUtils.compoundTime(TimeUtils.DEF_CHECK_IN_HOUR, TimeUtils.DEF_CHECK_OUT_MIN);
+    private long latestCheckInTime = TimeUtils.compoundTime(TimeUtils.DEF_LATEST_CHECK_IN_HOUR, TimeUtils.DEF_CHECK_OUT_MIN);
+    private long checkOutTime = TimeUtils.compoundTime(TimeUtils.DEF_CHECK_OUT_HOUR, TimeUtils.DEF_CHECK_OUT_MIN);
+
+    private void onClickTime(TimePickerDialog.OnTimeSetListener listener, long refTime) {
+        LatestCheckInTimePickerListener checkInTimePickerListener = new LatestCheckInTimePickerListener();
+
+        int hour = TimeUtils.getHour(refTime);
+        int min = TimeUtils.getMinute(refTime);
         TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(),
-                checkInTimePickerListener, 9,
-                0,
+                listener, hour,
+                min,
                 true);
         timePickerDialog.show();
-
     }
 }
