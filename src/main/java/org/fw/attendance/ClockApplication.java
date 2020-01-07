53a25acc3e580f886a66e42307d2836039f0eee2
Thomas Liu <liuxk99@gmail.com>
2020-01-07 18:43:12 +0800

    <feature: provider>: client code for provider.

--- src/main/java/org/fw/attendance/ClockApplication.java
+++ src/main/java/org/fw/attendance/ClockApplication.java
@@ -1,11 +1,29 @@
 package org.fw.attendance;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.sj.attendance.bl.FixWorkTimePolicy;
+import com.sj.attendance.bl.WorkTimePolicyFactory;
+import com.sj.attendance.provider.WorkTimePolicyAdapter;
+
+import java.util.LinkedList;
 
 public class ClockApplication extends Application {
+
+    private static final String TAG = ClockApplication.class.getSimpleName();
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        WorkTimePolicyAdapter adapter = new WorkTimePolicyAdapter(this);
+
+        adapter.insert(WorkTimePolicyFactory.generateFlexPolicy());
+
+        LinkedList<FixWorkTimePolicy> policyList = adapter.getAll();
+        for (FixWorkTimePolicy policy: policyList) {
+            Log.d(TAG, policy.toString());
+        }
     }
 }
